230a19288bf8b471c691833333e21b62b62b122e
MeZeksan <bulzak74848@gmail.com>
2025-12-10 21:44:14 +0500

    crud операции

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import Settings
 import uvicorn
 
 app = FastAPI()
+
+# Создаем экземпляр настроек
+settings = Settings()
+
+@app.on_event("startup")
+async def init_db():
+    await settings.initialize_database()
+
 # Register routes
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
 
